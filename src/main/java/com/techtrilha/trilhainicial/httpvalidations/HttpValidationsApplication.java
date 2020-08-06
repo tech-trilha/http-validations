8081683c97bc5ee8567435b2587efa24b1b0a4d8
Felipe Miranda <felip.simi@gmail.com>
2020-08-05 21:22:24 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/trilhainicial/httpvalidations/HttpValidationsApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.httpvalidations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpValidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpValidationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtrilha/trilhainicial/httpvalidations/HttpValidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.httpvalidations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpValidationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

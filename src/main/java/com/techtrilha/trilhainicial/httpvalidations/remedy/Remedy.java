9693fa2db88c31615d38eabacc13886a743c2620
Felipe Miranda <felip.simi@gmail.com>
2020-08-08 16:34:36 -0300

    Segundo passo: inclusao de validacoes na classe de remedios

--- src/main/java/com/techtrilha/trilhainicial/httpvalidations/remedy/Remedy.java
+++ src/main/java/com/techtrilha/trilhainicial/httpvalidations/remedy/Remedy.java
@@ -1,9 +1,17 @@
 package com.techtrilha.trilhainicial.httpvalidations.remedy;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public class Remedy {
 
+  @Positive
   private Long id;
+  @NotBlank
+  @Size(min = 2, max = 100)
   private String name;
+  @Size(max = 500)
   private String description;
 
   public Long getId() {

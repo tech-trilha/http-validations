95ae53914139cff3fe808b1485d327fea83014ba
Felipe Miranda <felip.simi@gmail.com>
2020-08-08 16:36:58 -0300

    Terceiro passo: validador na classe controller

--- src/main/java/com/techtrilha/trilhainicial/httpvalidations/control/RemedyController.java
+++ src/main/java/com/techtrilha/trilhainicial/httpvalidations/control/RemedyController.java
@@ -4,12 +4,13 @@ import com.techtrilha.trilhainicial.httpvalidations.remedy.Remedy;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
 
 @RestController
 public class RemedyController {
 
   @PostMapping("/remedies")
-  public Remedy generateRemedy(@RequestBody Remedy remedy) {
+  public Remedy generateRemedy(@Valid @RequestBody Remedy remedy) {
     remedy.setId(1L);
     return remedy;
   }
